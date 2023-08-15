0097d8832f0612b0d6d26323d1f62b2e6d3ca198
viprko <pr.and.vi@gmail.com>
2023-08-15 13:13:15 +0300

    created book entity and its repository, service

---
+++ src/main/java/mate/academy/bookstore/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package mate.academy.bookstore.repository;
+
+import mate.academy.bookstore.model.Book;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends CrudRepository<Book, Long> {
+}
